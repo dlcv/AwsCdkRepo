fe36101ddc63ea3bd8e8784da9aefd48b297fc60
fdelacruz-at-092623405260 <dlcv@live.com.ve>
2022-01-14 10:43:51 -0400

    Initial commit

---
+++ README_CHALLENGE.md
@@ -0,0 +1,19 @@
+# Llegó el momento de enfrentar un nuevo reto
+
+¡Hola, Francisco ¿Cómo estás?
+
+Nos encantó tu desempeño en la entrevista así que te invitamos a continuar en el proceso de selección.  
+
+Para ello, te invito a realizar el desafío donde evaluaremos tus habilidades técnicas.
+
+## Reto: 
+Desarrolla un proyecto utilizando el CDK de AWS y Typescript. Este módulo deberá incluir una lambda, que debe crear 2 archivos JSON que contengan un a propiedad status que sea de tipo string.
+
+•	Puntos extra por cada servicio de AWS utilizado (SSM, Secrets Manager, SQS, SNS, etc...)
+•	Puntos extra si se utiliza una estructura de datos tipo Map o Set.
+
+## Plazo para resolver el reto:
+Comienza: hoy miércoles 12/01/2022
+Finaliza: viernes 14/01/2022 a media noche.
+
+¡Esperamos que los disfrutes! Exitos 👾
--- lib/fdelacruz-hackmetrix-stack.ts
+++ lib/fdelacruz-hackmetrix-stack.ts
@@ -1,16 +1,87 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as kms from 'aws-cdk-lib/aws-kms';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import { SnsDestination } from 'aws-cdk-lib/aws-s3-notifications';
+import { assert } from 'console';
 
 export class FdelacruzHackmetrixStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Base name template
+    let baseName = 'AwsCdk';
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FdelacruzHackmetrixQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define a queue for notifications
+    const myQueue = new sqs.Queue(this, baseName + 'Queue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
+    // Define a topic for notifications
+    const myTopic = new sns.Topic(this, baseName + 'Topic');
+
+    // Add topic to queue for notifications
+    myTopic.addSubscription(new subs.SqsSubscription(myQueue));
+
+    // Define a role for lambda
+    const myRole = new iam.Role(this, baseName + 'LambdaRole', {
+      roleName: baseName + 'LambdaRole',
+      description: baseName + 'LambdaRole',
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole')
+    ]
+    });
+
+    // Define a lambda function
+    const myLambda = new lambda.Function(this, baseName + 'Lambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      timeout: cdk.Duration.seconds(300),
+      functionName: baseName + 'Lambda',
+      handler: 'mylambda.handler',
+      memorySize: 1024,
+      role: myRole
+    });
+
+    // Define a API gateway for handle request
+    new apigw.LambdaRestApi(this, baseName + 'API', {
+      handler: myLambda
+      // handler: myLambdaCounter.handler
+    });
+
+    // Define a KMS for encryptation
+    const myKmsKey = new kms.Key(this, baseName + 'KMS');
+
+    // Define a S3 bucket for save JSON files
+    const myBucket = new s3.Bucket(this, baseName + 'S3', {
+      versioned: true,
+      autoDeleteObjects: true,
+      encryption: s3.BucketEncryption.KMS,
+      encryptionKey: myKmsKey,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    assert(myBucket.encryptionKey === myKmsKey);
+
+    // Allow lambda read and write in the S3 bucket
+    // myBucket.grantReadWrite(myLambda);
+
+    // Add repository for CDK code
+    new codecommit.Repository(this, baseName + 'Repo', {
+      repositoryName: baseName + 'Repo',
+      description: 'Repository for ' + baseName + ' code',
+    });
+
   }
 }
